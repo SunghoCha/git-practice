d4566ddf249fe825e51cf68084417ba16ee3e9f5
SunghoCha <tjdgh3725@gmail.com>
2024-06-30 16:54:26 +0900

    main에서수정한내용

--- git_prac01/src/main/java/com/example/git_prac01/GitTest.java
+++ git_prac01/src/main/java/com/example/git_prac01/GitTest.java
@@ -2,7 +2,7 @@ package com.example.git_prac01;
 
 public class GitTest {
 
-    private String word = "깃테스트용";
+    private String word = "메인에서수정함";
     private String second = "2번째 내용";
     private String word3 = "3번째 내용";
     private String word4 = "네번째 내용";
