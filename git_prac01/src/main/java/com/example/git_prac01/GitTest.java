56b368c0a96a5b48f422d3517a29a87e0d1fe819
SunghoCha <tjdgh3725@gmail.com>
2024-06-30 15:47:11 +0900

    test2 - string add

--- git_prac01/src/main/java/com/example/git_prac01/GitTest.java
+++ git_prac01/src/main/java/com/example/git_prac01/GitTest.java
@@ -3,6 +3,7 @@ package com.example.git_prac01;
 public class GitTest {
 
     private String word = "깃테스트용";
-    private String second = "두번째 내용";
-    private String word3 = "세번째 내용";
+    private String second = "2번째 내용";
+    private String word3 = "3번째 내용";
+    private String word4 = "네번째 내용";
 }
